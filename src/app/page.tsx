f4d8119052b000116081fffdf457e5ea5c01984d
yuuta watanabe <yuutawatanabe@myBook.local>
2025-08-25 23:11:29 +0900

    Fix dynamic render error again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic"; // ★★★ この行を追加 ★★★
 
 import { useState } from "react";
 import { useSession, signIn, signOut } from "next-auth/react";
